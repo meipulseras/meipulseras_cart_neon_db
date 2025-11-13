7f1a903722bab401d15229e23c9708073cc86f72
wyneg <wynegsrhuntar@gmail.com>
2025-11-13 10:58:43 -0300

    samesite y secure de nuevo

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ app.use(session({
     cookie: {
         maxAge: (60 * 60 * 1000),
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        partitioned: true,
         path: "/"
     }
 }));
