ffbc17cae746ac4dd49663621bf3185abf9b927f
Wyneg S. Rhuntar <wynegsrhuntar@gmail.com>
2026-03-10 21:16:20 -0300

    se da padding a icono de inicio de sesion

--- public/js/hide.js
+++ public/js/hide.js
@@ -4,6 +4,7 @@ if(username !== null && username.innerText == ''){
     document.getElementById("counter").style.display= "none";
     document.getElementById("cart").style.display= "none";
     document.getElementById('login').style.display = 'block';
+    document.getElementById('login').style.paddingRight = '15px';
     document.getElementById('logout').style.display = 'none';
     document.getElementById('pipe').style.display = 'none';
     document.getElementById("username").style.display = 'none';
