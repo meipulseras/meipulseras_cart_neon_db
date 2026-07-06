fc8c9d08b414c3812618011ba605946e12b5c79d
wyneg <wynegsrhuntar@gmail.com>
2026-07-06 11:18:28 -0400

    cambio para redis en caso de que redis de timeout

--- middleware/redisClient.js
+++ middleware/redisClient.js
@@ -2,15 +2,16 @@ import { createClient } from 'redis';
 
 const redisClientInstance = createClient({
     url: process.env.REDIS_URL,
+    disableOfflineQueue: true,
     socket: {
-        connectTimeout: 10000,
+        connectTimeout: 2000,
         keepAlive: 5000,
         reconnectStrategy: (retries) => {
-            if(retries >= 10) {
+            if (retries >= 5) {
                 return new Error('Se completó la cantidad límite de reconexiones');
             }
 
-            return Math.min(retries * 100, 5000);
+            return Math.min(retries * 500, 2000);
         }
     }
 });
